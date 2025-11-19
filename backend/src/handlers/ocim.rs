ae8c28f7a4bf6772ed4c96518ecee155b597d5fa
MauriceChartee <maurice.chartee@rwth-aachen.de>
2025-11-19 13:53:36 +0100

    changed ocim handler to have expected json output

--- backend/src/handlers/ocim.rs
+++ backend/src/handlers/ocim.rs
@@ -3,12 +3,15 @@ use axum::{response::IntoResponse, http::StatusCode};
 use crate::models::ocel::OCEL;
 use crate::traits::import_export::ImportableFromPath;
 use crate::core::ocim::algorithm::ocim_init;
+use crate::core::struct_converters::ocpt_frontend_backend::backend_to_frontend;
+
 
 
 pub async fn apply_ocim(Path(file_id): Path<String>) -> Result<impl IntoResponse, (StatusCode, String)> {
     let ocel = OCEL::import_from_path(&file_id).await?;
 
     let ocpt = ocim_init(&ocel);
+    let ocpt_frontend = backend_to_frontend(&ocpt); //needed to add this step since frontend has a different ocpt format, than we use in the backend
 
-    Ok(axum::Json(ocpt))
+    Ok(axum::Json(ocpt_frontend))
 }
